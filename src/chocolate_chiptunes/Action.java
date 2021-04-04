1fe9e09977ddbe3b5e161fa4740a6f64232ee382
nscalese <n.scalese@yahoo.com>
2021-04-04 10:45:43 -0400

    Altered what the action class stored as values
    Changed the Strings to Objects, and added methods to invoke

--- src/chocolate_chiptunes/Action.java
+++ src/chocolate_chiptunes/Action.java
@@ -1,26 +1,40 @@
 package chocolate_chiptunes;
 
+import java.lang.reflect.Method;
+
 public class Action {
 
-	private String oldValue;
-	private String newValue;
+	private Object oldValue;
+	private Object newValue;
 	private Object objectAffected;
+	private Method objectMethodToInvoke;
+	private Method controllerMethodToInvoke;
 	
-	Action(String oldValue, String newValue, Object objectAffected) {
+	Action(Object oldValue, Object newValue, Object objectAffected, Method objectMethodToInvoke, Method controllerMethodToInvoke) {
 		this.oldValue = oldValue;
 		this.newValue = newValue;
 		this.objectAffected = objectAffected;
+		this.objectMethodToInvoke = objectMethodToInvoke;
+		this.controllerMethodToInvoke = controllerMethodToInvoke;
 	}
 	
-	public String getOldValue() {
+	public Object getOldValue() {
 		return oldValue;
 	}
 	
-	public String getNewValue() {
+	public Object getNewValue() {
 		return newValue;
 	}
 	
 	public Object getObjectAffected() {
 		return objectAffected;
+	}
+	
+	public Method getObjectMethodToInvoke() {
+		return objectMethodToInvoke;
+	}
+	
+	public Method getControllerMethodToInvoke() {
+		return controllerMethodToInvoke;
 	}
 }
